48413c8ec229825f4e66d673e4d4cb8d3e0f97f9
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-03-08 09:18:53 +0200

    corrects the mentor's remarks

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Container } from 'components/BasicStyled/Container.styled';
 import { FriendListItem } from 'components/FriendList/FriendListItem';
 
@@ -5,8 +6,28 @@ export const FriendList = ({ friends }) => {
   return (
     <Container>
       <ul>
-        <FriendListItem friends={friends} />
+        {friends.map(({ avatar, name, isOnline, id }) => {
+          return (
+            <FriendListItem
+              key={id}
+              avatar={avatar}
+              name={name}
+              isOnline={isOnline}
+            />
+          );
+        })}
       </ul>
     </Container>
   );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
 };
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,25 +1,11 @@
-import PropTypes from 'prop-types';
 import { FriendItem, Marker } from './FriendList.styled';
 
-export const FriendListItem = ({ friends }) => {
-  return friends.map(({ avatar, name, isOnline, id }) => {
-    return (
-      <FriendItem key={id}>
-        <Marker isOnline={isOnline}></Marker>
-        <img src={avatar} alt="User avatar" />
-        <p>{name}</p>
-      </FriendItem>
-    );
-  });
-};
-
-FriendListItem.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.exact({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ),
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <FriendItem>
+      <Marker isOnline={isOnline}></Marker>
+      <img src={avatar} alt="User avatar" />
+      <p>{name}</p>
+    </FriendItem>
+  );
 };
